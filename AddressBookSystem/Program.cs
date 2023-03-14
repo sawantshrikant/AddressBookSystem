eaa1c9cb1c0a9b55bb24c28a6ea89d88d4e12050
Shrikant <sawantshrikant07@gmail.com>
2023-03-14 22:55:42 +0530

    [Shrikant] add:AbilityToCreateAContactsInAddressBook

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
-
 
 namespace AddressBook
 {
@@ -9,9 +7,37 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program!");
-
+            Contact contact = new Contact();
+            Console.WriteLine("Enter First Name:");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name:");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address:");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City:");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State:");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter PostalCode:");
+            contact.PostalCode = Console.ReadLine();
+            Console.WriteLine("Enter PhoneNumber:");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email:");
+            contact.Email = Console.ReadLine();
+            DisplayContact(contact);
         }
 
-
+        static void DisplayContact(Contact contact)
+        {
+            Console.WriteLine("\nDisplaying Contact Information:");
+            Console.WriteLine("First Name: " + contact.FirstName);
+            Console.WriteLine("Last Name: " + contact.LastName);
+            Console.WriteLine("Address: " + contact.Address);
+            Console.WriteLine("City: " + contact.City);
+            Console.WriteLine("State: " + contact.State);
+            Console.WriteLine("Postal Code: " + contact.PostalCode);
+            Console.WriteLine("Phone Number: " + contact.PhoneNumber);
+            Console.WriteLine("Email: " + contact.Email);
+        }
     }
 }
