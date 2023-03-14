64f475518a2c20f293574a3a810da1d0da3d50e6
Shrikant <sawantshrikant07@gmail.com>
2023-03-14 22:45:46 +0530

    [Shrikant] add:WelcomeToAddressBookSystemProgram

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program!");
+
+        }
+
+
+    }
+}
